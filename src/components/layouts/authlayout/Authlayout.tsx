ddc8a486531d6da4f72916812e6ab7f8f1b097d3
Sandeep Kumar <sandeep.asvayuktech@gmail.com>
2025-09-23 12:17:18 +0530

    Modification in pages and components

--- src/components/layouts/authlayout/Authlayout.tsx
+++ src/components/layouts/authlayout/Authlayout.tsx
@@ -42,7 +42,7 @@ const Authlayout = () => {
             <div className="mx-auto max-w-screen-2xl px-3 sm:px-10">
                 <div className="py-4 flex flex-col lg:flex-row w-full">
                     <div className="w-full sm:p-5 lg:p-8">
-                        <div className="mx-auto text-left justify-center rounded-md w-full max-w-lg px-4 py-8 sm:p-10 overflow-hidden align-middle transition-all transform bg-white shadow-xl rounded-2xl">
+                        <div className="mx-auto text-left justify-center w-full max-w-lg px-4 py-8 sm:p-10 overflow-hidden align-middle transition-all transform bg-white shadow-xl rounded-2xl">
                             <div className="overflow-hidden mx-auto">
                                 {/* Page Header */}
                                 <div className="text-center mb-6">
