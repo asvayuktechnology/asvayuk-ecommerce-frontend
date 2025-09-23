fbd145bfbc3d030399fbca7e85be0dd07f0c1c4e
Sandeep Kumar <sandeep.asvayuktech@gmail.com>
2025-09-23 12:51:48 +0530

    create password show and hide

--- src/components/forms/loginform/Loginform.tsx
+++ src/components/forms/loginform/Loginform.tsx
@@ -1,15 +1,17 @@
-import React from "react";
+import React, { useState } from "react";
 import Link from "next/link";
 import { useForm } from "react-hook-form";
 import { loginSchema } from "@/schemas/authSchemas/authSchemas";
 import { zodResolver } from "@hookform/resolvers/zod";
 import CustomInput from "@/components/ui/common/inputs/CustomInput";
 import { TbMailBitcoin } from "react-icons/tb";
-import { RiLockPasswordLine } from "react-icons/ri";
+import { RiEyeLine, RiEyeOffLine, RiLockPasswordLine } from "react-icons/ri";
 import z from "zod";
 type loginSchemaData = z.infer<typeof loginSchema>;
 
 const Loginform = () => {
+  const [showPassword, setShowPassword] = useState(false);
+
   const {
     register,
     handleSubmit,
@@ -42,13 +44,26 @@ const Loginform = () => {
               </span>
             )}
           </div>
-          <div className="form-group">
+          <div className="form-group relative">
             <CustomInput
-              {...register("password")}
+              {...register("password", { required: "Password is required" })}
               label="Password"
               placeholder="Password"
+              type={showPassword ? "text" : "password"}
               icon={<RiLockPasswordLine className="w-5 h-5 text-gray-500" />}
             />
+            <button
+              type="button"
+              className="absolute right-3 top-[45px] text-gray-500 cursor-pointer"
+              onClick={() => setShowPassword(!showPassword)}
+            >
+              {showPassword ? (
+                <RiEyeOffLine className="w-4 h-4" />
+              ) : (
+                <RiEyeLine className="w-4 h-4" />
+              )}
+            </button>
+
             {errors.password && (
               <span className="text-red-400 text-sm mt-2">
                 {errors.password.message}
